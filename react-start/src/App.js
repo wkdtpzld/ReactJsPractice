252a03f3a70636bee95552bfe916016a81895305
wkdtpzld <87063105+wkdtpzld@users.noreply.github.com>
2022-08-16 17:52:08 +0900

    react-start-app Default Setting

---
+++ react-start/src/App.js
@@ -0,0 +1,13 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className={styles.title}>Welcome Back!</h1>
+      <Button text="TempText"/>
+    </div>
+  );
+}
+
+export default App;
