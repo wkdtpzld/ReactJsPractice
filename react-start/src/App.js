aeb83d6eb36b85f41644d7aeb07cb32eb44b0562
wkdtpzld <87063105+wkdtpzld@users.noreply.github.com>
2022-08-16 22:44:06 +0900

    React useEffect Practice

--- react-start/src/App.js
+++ react-start/src/App.js
@@ -1,11 +1,29 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-
+import { useState, useEffect } from "react";
+  
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  console.log("i run all the time");
+  
+  useEffect(() => {
+    console.log("CALL THE AN API");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text="TempText"/>
+      <input value={ keyword } type="text" placeholder="Search here..." onChange={onChange} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click Me</button>
     </div>
   );
 }
