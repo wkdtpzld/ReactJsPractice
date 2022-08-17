fe657fd2416070a7dc5e1bb0be4e6c2107a6e78f
wkdtpzld <87063105+wkdtpzld@users.noreply.github.com>
2022-08-17 13:36:37 +0900

    CodeChallenge State And useEffect

--- react-start/src/App.js
+++ react-start/src/App.js
@@ -1,30 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  }
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [input, setInput] = useState("");
+  const [nowCoin, setNowCoin] = useState();
 
-  console.log(toDos);
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const onSelectCoin = (event) => {
+    setNowCoin(event.target.value);
+  };
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => {
+      setCoins(json);
+      setNowCoin(json[0].quotes.USD.price);
+      setLoading(false);
+    });
+  }, []);
 
   return <div>
-          <form onSubmit={onSubmit}>
-            <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do"/>
-            <button>Add To Do</button>
-          </form>
-          <hr />
-          <ul>
-            {toDos.map((item, index) => <li key={index}>{item}</li>)}
-          </ul>
+          <h1>The Coins!({coins.length})</h1>
+          
+          {loading ? <strong>Loading...</strong> :
+            <div>
+              <input type="text" value={input} onChange={onChange}/>
+              <br />
+              <select value={nowCoin} onChange={onSelectCoin}>
+                {coins.map((item) => 
+                  <option value={item.quotes.USD.price}>
+                    {item.name} ({item.symbol}) : {item.quotes.USD.price} USD
+                  </option>)}
+              </select>
+
+              <h1>Convert USD To Coin = {input / nowCoin}</h1>
+            </div>
+          }
         </div>;  
 }
 
